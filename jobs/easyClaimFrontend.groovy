d847b11595da46e988844b41c544db1507af5301
Vignesh Sweekaran <40670015+vigneshsweekaran@users.noreply.github.com>
2019-12-30 21:56:57 +0530

    Added the trigger step

--- jobs/easyClaimFrontend.groovy
+++ jobs/easyClaimFrontend.groovy
@@ -1,4 +1,8 @@
 pipelineJob('EasyClaimFrontend') {
+    triggers {
+        scm('* * * * *')
+    }
+    
     definition {
         cps {
             script(readFileFromWorkspace('pipeline/frontend-jenkinsfile'))

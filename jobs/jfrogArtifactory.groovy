0f5c3489a7de2be5f0e6a8c61582af860905c80f
Vignesh Sweekaran <40670015+vigneshsweekaran@users.noreply.github.com>
2020-01-01 13:35:29 +0530

    Update jfrogArtifactory.groovy

--- jobs/jfrogArtifactory.groovy
+++ jobs/jfrogArtifactory.groovy
@@ -1,8 +1,10 @@
-pipelineJob('JFrogArtifactory') {
-    definition {
-        cps {
-            script(readFileFromWorkspace('pipeline/jfrogArtifactory-jenkinsfile'))
-            sandbox()
-        }
+Job('JFrogArtifactory') {
+    description "Builds MyProject from master branch."
+    label('master')
+    steps {
+        shell "docker run --name artifactory -d -p 8081:8081 \
+            -v /data/jfrog/artifactory:/var/opt/jfrog/artifactory \
+            -e EXTRA_JAVA_OPTIONS='-Xms512m -Xmx1g -Xss256k -XX:+UseG1GC' \
+            docker.bintray.io/jfrog/artifactory-cpp-ce:latest"
     }
 }

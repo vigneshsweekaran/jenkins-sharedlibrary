f75df4cbbf3246db755c033eb8a1d290618c7bd5
Vignesh Sweekaran <40670015+vigneshsweekaran@users.noreply.github.com>
2020-01-01 22:51:47 +0530

    Update jfrogArtifactory.groovy

--- jobs/jfrogArtifactory.groovy
+++ jobs/jfrogArtifactory.groovy
@@ -10,6 +10,6 @@ job('JFrogArtifactory') {
         shell "docker run --name artifactory -d -p 8081:8081 \
             -v /data/jfrog/artifactory:/var/opt/jfrog/artifactory \
             -e EXTRA_JAVA_OPTIONS='-Xms512m -Xmx1g -Xss256k -XX:+UseG1GC' \
-            docker.bintray.io/jfrog/artifactory-cpp-ce:latest"
+            docker.bintray.io/jfrog/artifactory-oss:latest"
     }
 }

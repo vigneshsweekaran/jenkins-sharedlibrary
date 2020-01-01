955ba2dbb7fc8e0159a8ba0b20b2870bc8d7a508
Vignesh Sweekaran <40670015+vigneshsweekaran@users.noreply.github.com>
2020-01-01 13:58:43 +0530

    Update jfrogArtifactory.groovy

--- jobs/jfrogArtifactory.groovy
+++ jobs/jfrogArtifactory.groovy
@@ -2,6 +2,11 @@ job('JFrogArtifactory') {
     description "Builds MyProject from master branch."
     label('master')
     steps {
+        shell "sudo mkdir -p /data/jfrog/artifactory || true"
+        shell "sudo useradd -u 1030 artifactory || true"
+        shell "sudo chown -R artifactory /data || true"
+        shell "docker stop artifactory || true"
+        shell "docker rm artifactory || true"
         shell "docker run --name artifactory -d -p 8081:8081 \
             -v /data/jfrog/artifactory:/var/opt/jfrog/artifactory \
             -e EXTRA_JAVA_OPTIONS='-Xms512m -Xmx1g -Xss256k -XX:+UseG1GC' \

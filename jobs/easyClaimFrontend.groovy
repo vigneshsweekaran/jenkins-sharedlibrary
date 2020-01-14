ecfcec009a2810a15172547b9f0297cb7df9c173
Vignesh Sweekaran <40670015+vigneshsweekaran@users.noreply.github.com>
2020-01-14 09:01:33 +0530

    Update easyClaimFrontend.groovy

--- jobs/easyClaimFrontend.groovy
+++ jobs/easyClaimFrontend.groovy
@@ -1,12 +1,24 @@
 pipelineJob('EasyClaimFrontend') {
+    description('EasyClaimFrontend application developed in Angular 7')
+
     triggers {
         scm('* * * * *')
     }
-    
+
     definition {
-        cps {
-            script(readFileFromWorkspace('pipeline/frontend-jenkinsfile'))
-            sandbox()
+        cpsScm {
+            lightweight(true)
+            scm{
+                git {
+                    branch('master')
+                    remote{
+                        url('https://github.com/vigneshsweekaran/capstone-easyclaim-frontend.git')
+                        credential('git_credential')
+                    }
+                    extensions { }
+                }
+            }
+            scriptPath('backend-jenkinsfile')
         }
     }
 }

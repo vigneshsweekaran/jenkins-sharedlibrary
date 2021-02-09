a065c0aee6aa402e4052db95a6eebad35144eff2
Vignesh Sweekaran <40670015+vigneshsweekaran@users.noreply.github.com>
2021-02-09 09:58:54 +0530

    Create checkFileChanges.groovy

---
+++ vars/checkFileChanges.groovy
@@ -0,0 +1,26 @@
+def call(list) {
+    for(int i=0; i < list.size(); i++) {
+        print(i)
+        print(currentBuild)
+        for (changeLogSet in currentBuild.changeSets) { 
+            print(changeLogSet)
+            print(changeLogSet.getItems())
+            for (entry in changeLogSet.getItems()) { // for each commit in the detected changes
+                for (file in entry.getAffectedFiles()) {
+                    print(file)
+                    print(file.getPath())
+                    if (file.getPath() =~ /^app1/) {
+                        print(list[i])
+                        stage(list[i]){
+                            steps {
+                                dir (list[i]) {
+                                    sh 'mvn clean package'
+                                }
+                            }    
+                        }
+                    }
+                }
+            }
+        }
+    }
+}

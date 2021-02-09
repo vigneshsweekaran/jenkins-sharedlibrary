11e7f0fadf4b59c0e407781b8e5548824a325e66
Vignesh Sweekaran <40670015+vigneshsweekaran@users.noreply.github.com>
2021-02-09 12:12:22 +0530

    Update checkFileChanges.groovy

--- vars/checkFileChanges.groovy
+++ vars/checkFileChanges.groovy
@@ -1,17 +1,11 @@
 @NonCPS
 def call(list) {
     for(int i=0; i < list.size(); i++) {
-        print(i)
-        print(currentBuild)
         for (changeLogSet in currentBuild.changeSets) { 
-            print(changeLogSet)
-            print(changeLogSet.getItems())
             for (entry in changeLogSet.getItems()) { // for each commit in the detected changes
                 for (file in entry.getAffectedFiles()) {
-                    print(file)
                     print(file.getPath())
                     if (file.getPath() =~ "${list[i]}") {
-                        print(list[i])
                         return list[i]
                     }
                 }

6740ea79a8abb128f5093a635349cf0f5ef54e28
vigneshsweekaran <vigneshsweekaran@gmail.com>
2021-06-13 14:41:49 +0530

    updated

--- vars/dockerBuildPush.groovy
+++ vars/dockerBuildPush.groovy
@@ -1,12 +1,18 @@
 def call(String dockerRegistryName = 'Dockerhub') {
 
-    if (dockerRegistryName == "Dockerhub") {}
+    if (dockerRegistryName == "Dockerhub") {
         docker.build("vigneshsweekaran/hello-world:${TAG}")
+
+        println("Publishing docker images to $dockerRegistryName")
+
         docker.withRegistry("https://registry.hub.docker.com", 'docker_credential') {
             docker.image("vigneshsweekaran/hello-world:${TAG}").push()
             docker.image("vigneshsweekaran/hello-world:${TAG}").push("latest")
-    } elseif (dockerRegistryName == "JfrogArtifactory") {
+    } else if (dockerRegistryName == "JfrogArtifactory") {
         docker.build("default-docker-local/hello-world:${TAG}")
+
+        println("Publishing docker images to $dockerRegistryName")
+
         docker.withRegistry('https://vigneshsweekaran.jfrog.io', 'artifactory-credential') {
             docker.image("default-docker-local/hello-world:${TAG}").push()
             docker.image("default-docker-local/hello-world:${TAG}").push("latest")
